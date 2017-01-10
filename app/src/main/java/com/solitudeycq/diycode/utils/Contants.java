442b36113a1bd5f11c6ceabc63eb071f5e488027
solitudeycq <solitudeycq@163.com>
2017-01-10 11:48:00 +0800

    Constants

---
+++ app/src/main/java/com/solitudeycq/diycode/utils/Contants.java
@@ -0,0 +1,13 @@
+package com.solitudeycq.diycode.utils;
+
+import com.solitudeycq.diycode.BuildConfig;
+
+/**
+ * Created by solitudeycq on 2017/1/10.
+ */
+
+public class Contants {
+  public static String DIYCODE_APPLICATION_ID = BuildConfig.DIYCODE_APPLICATION_ID;
+  public static String DIYCODE_PRIVATE_KEY = BuildConfig.DIYCODE_PRIVATE_KEY;
+
+}
