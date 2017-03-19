1a4017680e1f282c65eb3aa4b84142aa4c4b7d67
solitudeycq <solitudeycq@163.com>
2017-03-19 16:41:00 +0800

    占位文件

---
+++ app/src/main/java/com/solitudeycq/diycode/presenter/ILoginPresenter.java
@@ -0,0 +1,9 @@
+package com.solitudeycq.diycode.presenter;
+
+/**
+ * Created by solitudeycq on 2017/3/19.
+ * Email:solitudeycq@163.com
+ */
+
+public interface ILoginPresenter {
+}
---
+++ app/src/main/java/com/solitudeycq/diycode/widget/Nothing.java
@@ -0,0 +1,9 @@
+package com.solitudeycq.diycode.widget;
+
+/**
+ * Created by solitudeycq on 2017/3/19.
+ * Email:solitudeycq@163.com
+ */
+
+public class Nothing {
+}
